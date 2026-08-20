b96a5be8b4dc42cfa55bdc2acb60eb5b713cb9d2
Rajat Verma <cserajatverma@gmail.com>
2026-08-20 09:00:37 +0530

    Update routing configuration in app.routes.ts to include lazy loading for the layout module and a wildcard redirect. This change enhances navigation by ensuring the layout is loaded efficiently and provides a fallback route for unmatched paths, improving overall user experience within the application.

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./layout/layout.routes').then((m) => m.LAYOUT_ROUTES),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
