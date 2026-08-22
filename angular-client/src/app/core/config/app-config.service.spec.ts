0ce74678557670f97947e0e6ee7cc450255de92f
Rajat Verma <cserajatverma@gmail.com>
2026-08-22 22:00:34 +0530

    Fix import statement for TestBed in AppConfigService tests

--- angular-client/src/app/core/config/app-config.service.spec.ts
+++ angular-client/src/app/core/config/app-config.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { AppConfigService } from './app-config.service';
 import { APP_CONFIG, defaultConfig, stagingConfig } from './app-config';
 
