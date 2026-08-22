6b0bd3f09ddd73b9be227b3b2c42f1e014aea596
Rajat Verma <cserajatverma@gmail.com>
2026-08-22 22:24:56 +0530

    Refactor AUTH_ROUTES to include guestGuard and update routing structure

--- angular-client/src/app/features/auth/auth.routes.ts
+++ angular-client/src/app/features/auth/auth.routes.ts
@@ -1,8 +1,25 @@
 import { Routes } from '@angular/router';
+import { guestGuard } from './guards/guest.guard';
 
 export const AUTH_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./auth.component').then((m) => m.AuthComponent),
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    canActivate: [guestGuard],
+    loadComponent: () => import('./components/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    canActivate: [guestGuard],
+    loadComponent: () => import('./components/register/register.component').then((m) => m.RegisterComponent),
+  },
+  {
+    path: 'oauth/callback',
+    loadComponent: () =>
+      import('./components/oauth-callback/oauth-callback.component').then((m) => m.OauthCallbackComponent),
   },
 ];
