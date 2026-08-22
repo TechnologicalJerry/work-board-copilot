c307d0b499929cc6b8e6441d793027a9eab81cb3
Rajat Verma <cserajatverma@gmail.com>
2026-08-22 22:23:29 +0530

    Refactor authPlaceholderInterceptor to directly export from auth.interceptor

--- angular-client/src/app/core/http/interceptors/auth-placeholder.interceptor.ts
+++ angular-client/src/app/core/http/interceptors/auth-placeholder.interceptor.ts
@@ -1,18 +1,1 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { IS_PUBLIC_ENDPOINT } from '../http-context';
-
-/**
- * Extension point for Stage 3 Authentication.
- * In Stage 3, this interceptor will inject the auth token into the Authorization header
- * for non-public requests. In Stage 2, it acts as a transparent placeholder.
- */
-export const authPlaceholderInterceptor: HttpInterceptorFn = (req, next) => {
-  if (req.context.get(IS_PUBLIC_ENDPOINT)) {
-    return next(req);
-  }
-
-  // Extension point for Stage 3 Bearer Token insertion:
-  // e.g., req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
-
-  return next(req);
-};
+export { authInterceptor as authPlaceholderInterceptor, authInterceptor } from './auth.interceptor';
