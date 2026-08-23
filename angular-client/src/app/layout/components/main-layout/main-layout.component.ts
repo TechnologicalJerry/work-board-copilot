af390123b822128373946f9e8433181a68a5ec38
Rajat Verma <cserajatverma@gmail.com>
2026-08-23 12:51:53 +0530

    Refactor MainLayoutComponent to use AppShellComponent for layout management

--- angular-client/src/app/layout/components/main-layout/main-layout.component.ts
+++ angular-client/src/app/layout/components/main-layout/main-layout.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../navbar/navbar.component';
-import { SidebarComponent } from '../sidebar/sidebar.component';
+import { AppShellComponent } from '../app-shell/app-shell.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
-  template: `
-    <div class="min-h-screen bg-slate-950 text-slate-100 flex flex-col antialiased">
-      <app-navbar></app-navbar>
-
-      <div class="flex flex-1 overflow-hidden">
-        <app-sidebar></app-sidebar>
-
-        <main class="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8 bg-slate-950">
-          <router-outlet></router-outlet>
-        </main>
-      </div>
-    </div>
-  `,
+  imports: [AppShellComponent],
+  template: `<app-shell></app-shell>`,
 })
 export class MainLayoutComponent {}
