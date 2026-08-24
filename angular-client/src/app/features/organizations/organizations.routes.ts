4eddbeda0e689c63997e38d76439b4baded087cd
Rajat Verma <cserajatverma@gmail.com>
2026-08-24 09:48:14 +0530

    Refactor organization routes to use dedicated components for organization list, detail, and settings pages

--- angular-client/src/app/features/organizations/organizations.routes.ts
+++ angular-client/src/app/features/organizations/organizations.routes.ts
@@ -3,6 +3,23 @@ import { Routes } from '@angular/router';
 export const ORGANIZATIONS_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./organizations.component').then((m) => m.OrganizationsComponent),
+    loadComponent: () =>
+      import('./pages/organization-list-page/organization-list-page.component').then(
+        (m) => m.OrganizationListPageComponent
+      ),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./pages/organization-detail-page/organization-detail-page.component').then(
+        (m) => m.OrganizationDetailPageComponent
+      ),
+  },
+  {
+    path: ':id/settings',
+    loadComponent: () =>
+      import('./pages/organization-settings-page/organization-settings-page.component').then(
+        (m) => m.OrganizationSettingsPageComponent
+      ),
   },
 ];
