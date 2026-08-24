4d6e952b116bbd2f9daf1fbaea5c0bef8dd8be8a
Rajat Verma <cserajatverma@gmail.com>
2026-08-24 09:49:07 +0530

    Update SETTINGS_ROUTES to load UserSettingsPageComponent instead of SettingsComponent

--- angular-client/src/app/features/settings/settings.routes.ts
+++ angular-client/src/app/features/settings/settings.routes.ts
@@ -3,6 +3,9 @@ import { Routes } from '@angular/router';
 export const SETTINGS_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./settings.component').then((m) => m.SettingsComponent),
+    loadComponent: () =>
+      import('../users/pages/user-settings-page/user-settings-page.component').then(
+        (m) => m.UserSettingsPageComponent
+      ),
   },
 ];
