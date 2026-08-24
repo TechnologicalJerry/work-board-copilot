3174e60a8b515e319a53c82f9885f3191383a813
Rajat Verma <cserajatverma@gmail.com>
2026-08-24 09:44:56 +0530

    Add server routes for organizations, workspaces, and teams with client render mode

--- angular-client/src/app/app.routes.server.ts
+++ angular-client/src/app/app.routes.server.ts
@@ -2,7 +2,27 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'organizations/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'organizations/:id/settings',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'workspaces/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'workspaces/:id/settings',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'teams/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
