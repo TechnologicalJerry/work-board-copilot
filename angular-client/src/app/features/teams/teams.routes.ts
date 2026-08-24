bf37454139b71aacc7618865b44ad7cd5197e989
Rajat Verma <cserajatverma@gmail.com>
2026-08-24 09:49:13 +0530

    Refactor TEAMS_ROUTES to load TeamListPageComponent and TeamDetailPageComponent for improved team navigation

--- angular-client/src/app/features/teams/teams.routes.ts
+++ angular-client/src/app/features/teams/teams.routes.ts
@@ -3,6 +3,16 @@ import { Routes } from '@angular/router';
 export const TEAMS_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./teams.component').then((m) => m.TeamsComponent),
+    loadComponent: () =>
+      import('./pages/team-list-page/team-list-page.component').then(
+        (m) => m.TeamListPageComponent
+      ),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./pages/team-detail-page/team-detail-page.component').then(
+        (m) => m.TeamDetailPageComponent
+      ),
   },
 ];
