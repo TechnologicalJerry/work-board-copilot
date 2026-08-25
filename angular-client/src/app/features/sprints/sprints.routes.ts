f192ec11e50c729cd865d550a16bd3dd03bb22fb
Rajat Verma <cserajatverma@gmail.com>
2026-08-25 10:03:15 +0530

    Update SPRINTS_ROUTES to load new sprint-related components for improved navigation, including SprintPlanningPage and SprintDetailPage.

--- angular-client/src/app/features/sprints/sprints.routes.ts
+++ angular-client/src/app/features/sprints/sprints.routes.ts
@@ -3,6 +3,16 @@ import { Routes } from '@angular/router';
 export const SPRINTS_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./sprints.component').then((m) => m.SprintsComponent),
+    loadComponent: () =>
+      import('./pages/sprint-planning-page/sprint-planning-page.component').then(
+        (m) => m.SprintPlanningPageComponent
+      ),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./pages/sprint-detail-page/sprint-detail-page.component').then(
+        (m) => m.SprintDetailPageComponent
+      ),
   },
 ];
