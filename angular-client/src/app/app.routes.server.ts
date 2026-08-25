f4b3df62f8eab2575641cb2950bc18d71629a2ad
Rajat Verma <cserajatverma@gmail.com>
2026-08-25 10:25:37 +0530

    Add route for documents with dynamic ID, enabling client-side rendering for document details in the application.

--- angular-client/src/app/app.routes.server.ts
+++ angular-client/src/app/app.routes.server.ts
@@ -38,6 +38,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Client,
   },
   {
+    path: 'documents/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
