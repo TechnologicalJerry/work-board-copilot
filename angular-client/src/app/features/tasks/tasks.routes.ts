74a717054e50d29a407f44542cd82b398253f40b
Rajat Verma <cserajatverma@gmail.com>
2026-08-25 10:04:13 +0530

    Update TASKS_ROUTES to load new task-related components, including TaskListPageComponent and TaskDetailPageComponent, enhancing navigation for task management.

--- angular-client/src/app/features/tasks/tasks.routes.ts
+++ angular-client/src/app/features/tasks/tasks.routes.ts
@@ -3,6 +3,16 @@ import { Routes } from '@angular/router';
 export const TASKS_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./tasks.component').then((m) => m.TasksComponent),
+    loadComponent: () =>
+      import('./pages/task-list-page/task-list-page.component').then(
+        (m) => m.TaskListPageComponent
+      ),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./pages/task-detail-page/task-detail-page.component').then(
+        (m) => m.TaskDetailPageComponent
+      ),
   },
 ];
