b36b49508ea0391b344862a136d37a0e88220ef3
Rajat Verma <cserajatverma@gmail.com>
2026-08-25 10:02:07 +0530

    Update PROJECTS_ROUTES to load new project-related components for improved navigation, including ProjectListPage, ProjectDetailPage, and ProjectSettingsPage.

--- angular-client/src/app/features/projects/projects.routes.ts
+++ angular-client/src/app/features/projects/projects.routes.ts
@@ -3,6 +3,23 @@ import { Routes } from '@angular/router';
 export const PROJECTS_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./projects.component').then((m) => m.ProjectsComponent),
+    loadComponent: () =>
+      import('./pages/project-list-page/project-list-page.component').then(
+        (m) => m.ProjectListPageComponent
+      ),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./pages/project-detail-page/project-detail-page.component').then(
+        (m) => m.ProjectDetailPageComponent
+      ),
+  },
+  {
+    path: ':id/settings',
+    loadComponent: () =>
+      import('./pages/project-settings-page/project-settings-page.component').then(
+        (m) => m.ProjectSettingsPageComponent
+      ),
   },
 ];
