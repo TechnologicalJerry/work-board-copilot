4d357b5ea8bbd0dd445dc9f4abd19c35cd2b7bf7
Rajat Verma <cserajatverma@gmail.com>
2026-08-26 23:48:06 +0530

    refactor: update time tracking route to load TimeTrackingPageComponent instead of TimeTrackingComponent

--- angular-client/src/app/features/time-tracking/time-tracking.routes.ts
+++ angular-client/src/app/features/time-tracking/time-tracking.routes.ts
@@ -3,6 +3,9 @@ import { Routes } from '@angular/router';
 export const TIME_TRACKING_ROUTES: Routes = [
   {
     path: '',
-    loadComponent: () => import('./time-tracking.component').then((m) => m.TimeTrackingComponent),
+    loadComponent: () =>
+      import('./pages/time-tracking-page/time-tracking-page.component').then(
+        (m) => m.TimeTrackingPageComponent
+      ),
   },
 ];
